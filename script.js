c3b1f99df287fcac2467699abdaa4fc632529d4d
ANNEMARIE05 <annemarieagbonou@gmail.com>
2024-07-10 14:25:36 +0000

    first commit

---
+++ script.js
@@ -0,0 +1,56 @@
+// fonction classique
+
+// const myObj = {
+//     foo(){
+//         console.log(this)
+     // this vas retourné "myobj", this fait refference au context appelant
+//     }
+// }
+// les methodes sont les function qui sont situé dans un objets
+// this est un mot cle qui peut avoir plusieur definition
+
+// en fonction flecher le this fait refference a l objet window
+// const myobj ={
+//     a:"5",
+//     fuu: () =>{
+//         console.log(this);
+//     }
+// }
+
+// spread operateur sert a ajouter trois point devant la variable
+// const arr = [1,2,3]
+// console.log(...arr);
+// const arr2 = [...arr,4]
+
+// pour le rest operateur on declare les trois points dans les parametre
+
+// une function pure est une function qui retourne tout lr temps la meme chose lorsqu'on lui passe la meme choses en parametre et tout ce passe dans la fonction et qui ne touche pas a l'environnement exterieur
+// const ad = (a,b) => a+b
+
+// les fonction d'ordre superieur sont des founction qui prenne d'autre fonction en parametre , ou sa return une autre function , ou les deux
+
+// une fonction colback est une fonction qui est rappeler par une autre fonction  
+
+
+// fonction classique
+// function foo(){
+
+// }foo()
+ 
+
+// fonction flechee
+// const foo = () =>{
+
+// }
+// const add = (a,b) => a+b
+// const add = a => a
+
+// les methodes sont les fonctions situé dans les objets
+
+
+// npx create-react-app react app
+// installation de webpack
+// bundle
+
+// tous le monde utilise create "react-app" parce que c'est tres pratique
+// npm start va nous ouvrir un serveur local
